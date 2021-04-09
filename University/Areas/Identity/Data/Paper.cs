428259e0c5a82f342304118e18fdea09d47ff06a
ahmadnajdi1998 <ahmad-najdi1998@hotmail.com>
2021-04-09 09:21:41 +0300

    initial commit

---
+++ University/Areas/Identity/Data/Paper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace University.Areas.Identity.Data
+{
+    public class Paper
+    {
+
+        public int id { get; set; }
+        public int NumberCopies { get; set; }
+
+        public String Content { get; set; }
+
+        public String StudentMail { get; set; }
+    }
+}
---
+++ University/Areas/Identity/Data/UniversityContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using University.Areas.Identity.Data;
+
+namespace University.Data
+{
+    public class UniversityContext : IdentityDbContext<User>
+    {
+        public UniversityContext(DbContextOptions<UniversityContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public DbSet<University.Areas.Identity.Data.Paper> Paper { get; set; }
+    }
+}
